80c0527d370af9929b818d12286b9518bc8381dc
diulioz <diulioz.u@gmail.com>
2023-02-05 15:55:34 -0500

    FIN

--- calculadora/views.py
+++ calculadora/views.py
@@ -4,7 +4,13 @@ from django.http import HttpResponse
 
 #Operación por operación
 def inicio(request):
-    return HttpResponse("<h1>Aplicación Calculadora</h1>")
+    str = """<h1>Aplicación Calculadora</h1> <h3>DAVID CRIOLLO</h3><h3>VANESSA GUSTIN</h3> <h3>JUAN CAMILO INSUASTY</h3> <h3>COMO USAR: </h3>
+    <h3>FORMA 1: Después de la direccón y puerto del servidor escribir 'calculadora/operacion/operador1/operador2 </h3>
+    <h3>EJEMPLO: 'http://127.0.0.1:8000/calculadora/suma/2/3 </h3>
+    <h3>FORMA 2: Después de la direccón y puerto del servidor escribir 'calculadora/calcular/letra/operador1/operador2, donde letra puede ser:</h3>
+    <h3>s: suma     r: resta    m: multiplicación   d: división</h3>
+    <h3>EJEMPLO: 'http://127.0.0.1:8000/calculadora/calcular/d/100/20 </h3>"""
+    return HttpResponse(str)
 
 def suma(request, op1, op2):
     return HttpResponse(op1+op2)
